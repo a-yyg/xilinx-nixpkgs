69560888869767ea6c4bc26793f94a226d216fe6
Yuri Guimaraes <yuri.kgpps@gmail.com>
2024-08-15 04:07:47 +0900

    Add webmproject/libwebp

--- pkgs/libwebp/overlay.nix
+++ pkgs/libwebp/overlay.nix
@@ -1,3 +1,12 @@
-final: prev: {
+final: prev: rec {
   libwebp-jacklicn = prev.callPackage ./default.nix {};
+  libwebp-webmproject = libwebp-jacklicn.overrideAttrs (oldAttrs: rec {
+    version = "1.4.0";
+    src = prev.fetchFromGitHub {
+      owner = "webmproject";
+      repo = "libwebp";
+      rev = "v${version}";
+      sha256 = "sha256-OR/VzKNn3mnwjf+G+RkEGAaaKrhVlAu1e2oTRwdsPj8=";
+    };
+  });
 }
